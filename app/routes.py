e18b1c8e5ff0c6172753c5213ef78bb0a9966a2b
h404z <storyabouthoz@163.com>
2019-10-07 00:10:35 +0800

    尝试template render

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 # @Time  :2019/10/6
 # @Author: h404z
-
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "hello flask"
+    user = {"username": "NickHo"}
+    return render_template("index.html", title="Home", user=user)
 
 
 if __name__ == '__main__':
