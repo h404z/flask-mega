4474a01828e0dc3e4aa49069e6131c815634d00c
h404z <storyabouthoz@163.com>
2019-10-10 09:26:37 +0800

    换成用url_for的方式视图函数

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time  :2019/10/6
 # @Author: h404z
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -30,7 +30,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash("Login requested for user {}, remember me={}".format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for("index"))  # url_for(函数名)
     return render_template("login.html", title="Sign In", form=form)
 
 
