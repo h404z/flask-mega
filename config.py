89c23f59aa1705c4ece72a3e9aa60e1859122736
h404z <storyabouthoz@163.com>
2019-10-11 19:48:58 +0800

    增加User表及相关配置

--- config.py
+++ config.py
@@ -3,7 +3,11 @@
 # @Time  :2019/10/8 20:17
 # @Author: h404z
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRETE_KEY') or 'you-are-beautiful'
+
+    SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 禁用不需要的Flask-SQLAlchemy功能，即每次在数据库中进行更改时都会向应用程序发出信号。
