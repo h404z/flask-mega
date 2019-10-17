d064bb2cf0d0462e5655a083315de454d1213ea9
h404z <storyabouthoz@163.com>
2019-10-17 18:07:00 +0800

    microblog增加shell_context

--- microblog.py
+++ microblog.py
@@ -3,7 +3,14 @@
 # @Time  :2019/10/6
 # @Author: h404z
 
-from app import app
+from app import app, db
+from app.models import User, Posts
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Posts": Posts}
+
 
 if __name__ == '__main__':
     pass
